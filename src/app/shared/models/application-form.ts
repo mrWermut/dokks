dd8d6da8105947bf48b356d2ec80cc1c38f39b01
mrWermut <40657533+mrWermut@users.noreply.github.com>
2021-10-02 15:20:40 +0300

    raw models
    added basic models and enums

---
+++ src/app/shared/models/application-form.ts
@@ -0,0 +1,15 @@
+import {ApplicationFormPriority, ApplicationScope, Secrecy, ApplicationFormType, ApplicationFormState} from './enums';
+import {User} from './user';
+  header?: string;
+  type?: ApplicationFormType;
+  state: ApplicationFormState;
+  createDate?: Date;
+  body?: string;
+  author: User;
+  priority?: ApplicationFormPriority;
+  applicationScope?: ApplicationScope;
+  secrecy?: Secrecy;
+  executive: User;
+  signatures: Array<User>;
+
+}
---
+++ src/app/shared/models/enums.ts
@@ -0,0 +1,39 @@
+
+export enum  Secrecy {
+  TOP_SECRET,
+  SECRET,
+  PUBLIC_USE
+}
+
+export enum  ApplicationFormPriority {
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN
+}
+
+export enum ApplicationScope {
+  GLOBAL,
+  FEDERAl,
+  REGIONAL
+
+}
+
+export enum ApplicationFormType {
+  INTERNAL,
+  EXTERNAl
+}
+
+export enum ApplicationFormState {
+  CREATED,
+  PROCESSING,
+  DONE
+}
+
+export enum UserPermissions {
+   APPROVE,
+   CREATE,
+   EDIT,
+   REJECT,
+   LOGIN
+}
